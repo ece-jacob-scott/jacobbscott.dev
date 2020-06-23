757fb3c6142d0fa7b14b9ddcf69cde58a1fd2a52
Jacob Scott <jscott35@uic.edu>
2020-06-22 20:46:55 -0500

    Blog generator now creates files with hash for caching

--- rollup-plugin-blog-compiler.js
+++ rollup-plugin-blog-compiler.js
@@ -27,35 +27,59 @@ import marked from "marked";
 import { promisify } from "util";
 import config from "./blog.config";
 import path from "path";
+import crypto from "crypto";
+import glob from "glob";
 
 const readFileProm = promisify(fs.readFile);
 const writeFileProm = promisify(fs.writeFile);
+const unlinkProm = promisify(fs.unlink);
+const globProm = promisify(glob);
 
 let blogs = [];
+const blogTemplatePath = path.resolve("./templates/blog_template.html");
+const blogHomeTemplatePath = path.resolve("./templates/blog_home.html");
 
 /**
  * Takes a blog config element and creates an element to display on the blog
  * home page.
  */
-const blogIndexElement = ({ title, date, description, path }) => {
+const blogIndexElement = ({ title, date, description, path }, hash) => {
+  // ...find the actual name of the file from pageHashMap
   return `
     <p><a href="/blogs/${date.getFullYear()}/${title
     .toLowerCase()
     .split(" ")
-    .join("_")}.html">${title}</a>: ${
+    .join("_")}-${hash}.html">${title}</a>: ${
     date.getMonth() + 1
   }-${date.getDate()}-${date.getFullYear()}</p>
   `;
+};
+
+const createURL = ({ title, date, description, _ }, hash) => {
+  return path.resolve(
+    `./blogs/${date.getFullYear()}/${title
+      .toLowerCase()
+      .split(" ")
+      .join("_")}-${hash}.html`
+  );
 };
 
 export default {
   name: "blog-compiler",
   buildStart: async function (options) {
     blogs = config.blogs;
+    const blogFiles = await globProm("./blogs/**/*");
+    const deleteOps = [];
+    for (const oldFile of blogFiles) {
+      if (oldFile.includes(".html")) {
+        deleteOps.push(unlinkProm(oldFile));
+      }
+    }
     // watch all the blog files for changes
     for (const blog of blogs) {
       this.addWatchFile(blog.path);
     }
+    await Promise.all(deleteOps);
   },
   generateBundle: async function (options, bundle, isWrite) {
     /**
@@ -65,49 +89,38 @@ export default {
      * - blogIndexTemplate: template html page for the blog index page
      */
     const writeOps = [];
-    const blogTemplate = await readFileProm(
-      path.resolve("./templates/blog_template.html")
-    );
-    const blogIndexTemplate = await readFileProm(
-      path.resolve("./templates/blog_home.html")
-    );
+    const blogTemplate = (await readFileProm(blogTemplatePath)).toString();
+    const blogHomeTemplate = (
+      await readFileProm(blogHomeTemplatePath)
+    ).toString();
+    const blogIndexes = [];
 
-    // Transform config objects into the content for the index page
-    const indexContent = blogs.map(blogIndexElement).join("\n");
-
-    // Create a writing promise and push it on the writeops stack
-    writeOps.push(
-      writeFileProm(
-        path.resolve("./blogs/index.html"),
-        blogIndexTemplate
-          .toString()
-          .replace("<content-here></content-here>", indexContent, {
-            flag: "w+",
-          })
-      )
-    );
-
-    // Create all the blog pages from the config objects
-    // NOTE: blogs is populated with blog configs because the buildStart
-    //       function runs before the generateBundle function
+    // build blog pages
     for (const blog of blogs) {
-      const blogPath = blog.path;
-      // get the blog content in markdown
-      const contentMD = (await readFileProm(blogPath)).toString();
-      // replace the blog template with blog content after converting it into
-      // html from markdown
-      const contentHTML = blogTemplate
-        .toString()
-        .replace("<content-here></content-here>", marked(contentMD));
-
-      const blogURL = path.resolve(
-        `./blogs/${blog.date.getFullYear()}/${blog.title
-          .toLowerCase()
-          .split(" ")
-          .join("_")}.html`
+      const md5Hash = crypto.createHash("md5");
+      const blogContentMD = (await readFileProm(blog.path)).toString();
+      const blogContentHTML = blogTemplate.replace(
+        "<content-here></content-here>",
+        marked(blogContentMD)
       );
-      writeOps.push(writeFileProm(blogURL, contentHTML, { flag: "w+" }));
+      md5Hash.update(blogContentHTML);
+      const hash = md5Hash.digest("hex");
+      const blogURL = createURL(blog, hash);
+      writeOps.push(writeFileProm(blogURL, blogContentHTML, { flag: "w+" }));
+      blogIndexes.push(blogIndexElement(blog, hash));
     }
+
+    const indexPageHTML = blogHomeTemplate.replace(
+      "<content-here></content-here>",
+      blogIndexes.join("\n")
+    );
+
+    writeOps.push(
+      writeFileProm(path.resolve("./blogs/index.html"), indexPageHTML, {
+        flag: "w+",
+      })
+    );
+
     await Promise.all(writeOps);
   },
 };
